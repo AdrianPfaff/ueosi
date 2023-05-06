5fb53c681e921b12da5dfd05015f09c7d8b93eea
Adrian Pfaff <mail@adrian-pfaff.de>
2023-05-06 22:52:35 +0200

    properly added OSI

--- Source/ThirdParty/OSI/OSI.Build.cs
+++ Source/ThirdParty/OSI/OSI.Build.cs
@@ -6,80 +6,33 @@ using EpicGames.Core;
 
 public class OSI : ModuleRules
 {
-	public static string RelativeModulePath = "Plugins/UEOSI/Source/ThirdParty/OSI/";
-	public static string RelativeSourcePath = "Plugins/UEOSI/Source/ThirdParty/OSI/" + "/lib/";
 	public OSI(ReadOnlyTargetRules Target) : base(Target)
 	{
-		//do not manually comple source files using UBT
+		//do not manually compile source files using UBT
 		Type = ModuleType.External;
+
 		
-		//but require precompiling
-        PrecompileForTargets = PrecompileTargetsType.Any;
-
-        PublicPreBuildLibraries.Add(MakePlatformLibPath(Target));
-
 		if (Target.Platform == UnrealTargetPlatform.Win64)
 		{
-			// Add the import library
-			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "x64", "Release", "open_simulation_interface.lib"));
+			//does not work with this lib due to technical constraints. instead copy to project binary dir
+			//PublicDelayLoadDLLs.Add("libprotobuf.dll");
+			
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "Libraries",
+				"open_simulation_interface_static.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "Libraries",
+				"libprotobuf.lib"));
 
-			// Delay-load the DLL, so we can load it from the right place first
-			PublicDelayLoadDLLs.Add("open_simulation_interface.dll");
+			var Includes=Path.Combine(ModuleDirectory, "Includes");
+			PublicIncludePaths.Add(Includes);
 
-			// Ensure that the DLL is staged along with the executable
-			RuntimeDependencies.Add("$(PluginDir)/Binaries/ThirdParty/OSI/Win64/open_simulation_interface.dll");
-        }
-        else if (Target.Platform == UnrealTargetPlatform.Mac)
-        {
-            PublicDelayLoadDLLs.Add(Path.Combine(ModuleDirectory, "Mac", "Release", "open_simulation_interface.dylib"));
-            RuntimeDependencies.Add("$(PluginDir)/Source/ThirdParty/UEOSILibrary/Mac/Release/open_simulation_interface.dylib");
-        }
-        else if (Target.Platform == UnrealTargetPlatform.Linux)
-		{
-			string ExampleSoPath = Path.Combine("$(PluginDir)", "Binaries", "ThirdParty", "OSI", "Linux", "x86_64-unknown-linux-gnu", "open_simulation_interface.so");
-			PublicAdditionalLibraries.Add(ExampleSoPath);
-			PublicDelayLoadDLLs.Add(ExampleSoPath);
-			RuntimeDependencies.Add(ExampleSoPath);
+			RuntimeDependencies.Add(Path.Combine(ModuleDirectory, "Libraries",
+				"libprotobuf.dll"));
+
+			//workaround for cryptic error
+			PublicDefinitions.Add("GOOGLE_PROTOBUF_INTERNAL_DONATE_STEAL_INLINE");
+			
+			
 		}
+
 	}
-	
-	static public string MakePlatformLibPath(ReadOnlyTargetRules Target)
-	{
-		string Library = "/open_simulation_interface.lib";
-        
-		if (Target.Platform == UnrealTargetPlatform.Mac)
-		{
-			Library = "/open_simulation_interface.a";
-		}
-		if (Target.Platform == UnrealTargetPlatform.Linux)
-		{
-			Library = "/open_simulation_interface.so";
-		}
-        
-		string RelativePath = RelativeSourcePath + Target.Platform.ToString() + Library;
-
-		return Path.Combine(Target.ProjectFile.Directory.FullName, RelativePath);
-	}
-
-    static public string OSILibBuildStep(ReadOnlyTargetRules Target)
-    {
-        if (Target.Platform == UnrealTargetPlatform.Win64)
-        {
-            return RelativeModulePath + "build_osi.bat";
-        } /*
-        else if (Target.Platform == UnrealTargetPlatform.Mac)
-        {
-            return RelativeModulePath + "build_osi.csh";
-        } else if (Target.Platform == UnrealTargetPlatform.Linux)
-        {
-	        return RelativeModulePath + "build_osi.sh";
-        }
-        */
-        else
-        {
-            Log.WriteLine(LogEventType.Error, "CannotBuildOSILib for platform {0}. Either no build script defined or library is incompatible with platform.", Target.Platform.ToString());
-            return "";
-            
-        }
-    }
 }
--- Source/UEOSI/Private/UEOSI.cpp
+++ Source/UEOSI/Private/UEOSI.cpp
@@ -1,51 +1,23 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "UEOSI.h"
-#include "Core.h"
 #include "Modules/ModuleManager.h"
-#include "Interfaces/IPluginManager.h"
-#include "UEOSILibrary/ExampleLibrary.h"
+#include "OSI/Includes/osi_sensorview.pb.h"
 
 #define LOCTEXT_NAMESPACE "FUEOSIModule"
 
 void FUEOSIModule::StartupModule()
 {
-	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	
+	// Call the test function in the third party library that opens a message box
+	osi3::SensorView view;
+	auto camview=view.add_camera_sensor_view();
 
-	// Get the base directory of this plugin
-	FString BaseDir = IPluginManager::Get().FindPlugin("UEOSI")->GetBaseDir();
-
-	// Add on the relative location of the third party dll and load it
-	FString LibraryPath;
-#if PLATFORM_WINDOWS
-	LibraryPath = FPaths::Combine(*BaseDir, TEXT("Binaries/ThirdParty/UEOSILibrary/Win64/ExampleLibrary.dll"));
-#elif PLATFORM_MAC
-    LibraryPath = FPaths::Combine(*BaseDir, TEXT("Source/ThirdParty/UEOSILibrary/Mac/Release/libExampleLibrary.dylib"));
-#elif PLATFORM_LINUX
-	LibraryPath = FPaths::Combine(*BaseDir, TEXT("Binaries/ThirdParty/UEOSILibrary/Linux/x86_64-unknown-linux-gnu/libExampleLibrary.so"));
-#endif // PLATFORM_WINDOWS
-
-	ExampleLibraryHandle = !LibraryPath.IsEmpty() ? FPlatformProcess::GetDllHandle(*LibraryPath) : nullptr;
-
-	if (ExampleLibraryHandle)
-	{
-		// Call the test function in the third party library that opens a message box
-		ExampleLibraryFunction();
-	}
-	else
-	{
-		FMessageDialog::Open(EAppMsgType::Ok, LOCTEXT("ThirdPartyLibraryError", "Failed to load example third party library"));
-	}
 }
 
 void FUEOSIModule::ShutdownModule()
 {
-	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
-	// we call this function before unloading the module.
-
-	// Free the dll handle
-	FPlatformProcess::FreeDllHandle(ExampleLibraryHandle);
-	ExampleLibraryHandle = nullptr;
+	
 }
 
 #undef LOCTEXT_NAMESPACE
