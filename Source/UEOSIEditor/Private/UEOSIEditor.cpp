de8b10b3bac2699e65fd0947ed1fa62771cc9b31
Adrian Pfaff <mail@adrian-pfaff.de>
2023-05-25 10:45:29 +0200

    add editor module

---
+++ Source/UEOSIEditor/Private/UEOSIEditor.cpp
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UEOSIEditor.h"
+#include "Modules/ModuleManager.h"
+
+#define LOCTEXT_NAMESPACE "FUEOSIEditorModule"
+
+void FUEOSIEditorModule::StartupModule()
+{
+
+}
+
+void FUEOSIEditorModule::ShutdownModule()
+{
+	
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FUEOSIEditorModule, UEOSIEditor)
