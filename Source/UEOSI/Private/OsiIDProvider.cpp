f882b5df46b1ac84b90462fe7ba20f540a3a8f3c
Adrian Pfaff <mail@adrian-pfaff.de>
2023-06-13 14:28:16 +0200

    added osi id provider

---
+++ Source/UEOSI/Private/OsiIDProvider.cpp
@@ -0,0 +1,35 @@
+﻿// Copyright 2023 Adrian Pfaff
+
+#include "OsiIDProvider.h"
+
+bool FOsiIDProvider::AddReservedID(uint64 InID)
+{
+	FScopeLock ScopeLock(&Section);
+	if(InID<NextID || ReservedIDs.Contains(InID))
+	{
+		//ID cannot be reserved
+		return false;
+	}
+
+	if(InID==NextID)
+	{
+		++NextID;
+		return true;
+	}
+
+	ReservedIDs.Add(InID);
+	return true;
+}
+
+uint64 FOsiIDProvider::RequestID()
+{
+	FScopeLock ScopeLock(&Section);
+
+	return NextID++;
+}
+
+FOsiIDProvider& FOsiIDProvider::DefaultProvider()
+{
+	static FOsiIDProvider Singleton;
+	return Singleton;
+}
---
+++ Source/UEOSI/Public/OsiIDProvider.h
@@ -0,0 +1,29 @@
+﻿// Copyright 2023 Adrian Pfaff
+
+#pragma once
+
+
+class FOsiIDProvider
+{
+public:
+
+	//add an ID as reserved, returns true only if ID was not reserved yet and could be reserved
+	bool AddReservedID(uint64 InID);
+
+	uint64 RequestID();
+
+	static FOsiIDProvider& DefaultProvider();
+
+private:
+	
+	uint64 NextID=0;
+
+	mutable FCriticalSection Section;
+
+	TSet<uint64> ReservedIDs;
+
+	FOsiIDProvider()= default;
+	~FOsiIDProvider()= default;
+	FOsiIDProvider(const FOsiIDProvider&)= delete;
+	FOsiIDProvider& operator=(const FOsiIDProvider&)= delete;
+};
