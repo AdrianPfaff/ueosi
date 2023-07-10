47f0234f1a33d8a16ef8c2fe228537d99eefcb9b
Adrian Pfaff <mail@adrian-pfaff.de>
2023-07-10 10:35:40 +0200

    add occupant fields except classification

--- Source/UEOSI/Private/Declarations/Occupant.cpp
+++ Source/UEOSI/Private/Declarations/Occupant.cpp
@@ -3,3 +3,28 @@
 
 #include "Declarations/Occupant.h"
 
+#include "OsiIDProvider.h"
+#include "osi_groundtruth.pb.h"
+#include "OsiThread/OsiWorldSubsystem.h"
+
+void UOccupant::Initialize()
+{
+	auto GroundTruth=OsiSubsystem->GetGlobalGroundTruth();
+	DriverID=FOsiIDProvider::DefaultProvider().RequestID();
+	InternalOccupant=GroundTruth->add_occupant();
+
+	
+}
+
+void UOccupant::InitialDispatch()
+{
+	DispatchCommand([OsiOccupant=InternalOccupant, DriverID=DriverID]()
+	{
+		OsiOccupant->mutable_id()->set_value(DriverID);
+	});
+}
+
+void UOccupant::Update()
+{
+	
+}
--- Source/UEOSI/Public/Declarations/Occupant.h
+++ Source/UEOSI/Public/Declarations/Occupant.h
@@ -6,6 +6,11 @@
 #include "Declarations/ObjectDeclaration.h"
 #include "Occupant.generated.h"
 
+namespace osi3
+{
+	class Occupant;
+}
+
 /**
  * 
  */
@@ -13,5 +18,25 @@ UCLASS()
 class UEOSI_API UOccupant : public UObjectDeclaration
 {
 	GENERATED_BODY()
+
+public:
+
+	virtual void Initialize() override;
+
+	virtual void InitialDispatch() override;
 	
+	virtual void Update() override;
+
+protected:
+
+	osi3::Occupant* InternalOccupant;
+
+	//The ID of the driver.
+	uint64 DriverID;
+
+	//Specific information about the classification of the occupant.
+
+	//External reference to the occupant source. Note:For OpenDRIVE and OpenSCENARIO there is no direct counterpart. For non-ASAM Standards, it is implementation-specific how source_reference is resolved. The value has to be repeated because one object may be derived from more than one origin source, for example, from a scenario file and from sensors. 
+	//TODO: Add TArray<ExternalReference>
+
 };
